81ad3f3aa81471dd479fb02bf8f971baee0cf8d0
ThatCommand <g.vianello002@studenti.unibs.it>
2021-05-27 23:06:52 +0200

    Update Nome.java

--- src/ttt/tamagolem/contents/xml/elements/Nome.java
+++ src/ttt/tamagolem/contents/xml/elements/Nome.java
@@ -1,9 +1,9 @@
 package ttt.tamagolem.contents.xml.elements;
 
 import ttt.utils.engines.enums.MethodType;
+import ttt.utils.engines.interfaces.EngineMethod;
 import ttt.utils.xml.document.XMLElement;
 import ttt.utils.xml.engine.annotations.Element;
-import ttt.utils.xml.engine.annotations.EngineMethod;
 import ttt.utils.xml.engine.annotations.Tag;
 
 /**
